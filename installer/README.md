4b5d456b2961821208a9c155a8526d910edd8a16
noshdotzip <majorsboy1@gmail.com>
2026-02-22 00:29:22 -0500

    Update installer and docs for new repo

--- installer/README.md
+++ installer/README.md
@@ -1,5 +1,3 @@
 # Installer
 
-[![Illuaminate lint](https://img.shields.io/github/actions/workflow/status/CC-YouCube/installer/illuaminate-lint.yml?branch=main&label=Illuaminate%20lint&logo=github&style=for-the-badge)](https://github.com/CC-YouCube/installer/actions/workflows/illuaminate-lint.yml)
-
-Script that installs the YouCube client
+Script that installs the YouCube client.
--- src/youcube.lua
+++ src/youcube.lua
@@ -3,7 +3,7 @@ _   _ ____ _  _ ____ _  _ ___  ____
  \_/  |  | |  | |    |  | |__] |___
   |   |__| |__| |___ |__| |__] |___
 
-Github Repository: https://github.com/Commandcracker/YouCube
+Github Repository: https://github.com/REPLACE_OWNER/youcube-client
 License: GPL-3.0
 ]]
 
@@ -214,7 +214,7 @@ local audiodevices = get_audiodevices()
 -- update check --
 
 local function get_versions()
-    local url = "https://raw.githubusercontent.com/CC-YouCube/installer/main/versions.json"
+    local url = "https://raw.githubusercontent.com/REPLACE_OWNER/youcube-client/main/installer/versions.json"
 
     -- Check if the URL is valid
     local ok, err = http.checkURL(url)
